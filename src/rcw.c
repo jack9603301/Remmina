403113083cd37d8b86ae66ab5f88261a7aa0c90c
Giovanni Panozzo <giovanni@panozzo.it>
2019-12-22 17:24:15 +0100

    Better handling of focus out ungrab prevention

--- src/rcw.c
+++ src/rcw.c
@@ -515,6 +515,14 @@ static void rcw_keyboard_grab(RemminaConnectionWindow *cnnwin)
 		 * https://gitlab.gnome.org/GNOME/gtk/commit/726ad5a5ae7c4f167e8dd454cd7c250821c400ab
 		 * The bugfix will be released with GTK 3.24.
 		 * Also pease note that the newer gdk_seat_grab() is still calling gdk_device_grab().
+		 *
+		 * dont_ungrab_on_next_focus_out_event: Xorg when grabbing keyboard
+		 * generates an extra focus-out and focus-in event
+		 * https://stackoverflow.com/questions/15270420/why-xgrabkey-generates-extra-focus-out-and-focus-in-events
+		 * with mode field set to XCB_NOTIFY_MODE_GRAB
+		 * Some windows managers can detect this evend via the mode==XCB_NOTIFY_MODE_GRAB and suppress it.
+		 * Some WM don't. Under GTK we can't check the mode field, so we need a workaround to block
+		 * the spurious focus-out event we receive, setting dont_ungrab_on_next_focus_out_event to true.
 		 */
 		cnnwin->priv->dont_ungrab_on_next_focus_out_event = TRUE;
 #if GTK_CHECK_VERSION(3, 24, 0)
@@ -2398,8 +2406,8 @@ static void rcw_focus_out(RemminaConnectionWindow *cnnwin)
 
 	if (!cnnwin->priv->dont_ungrab_on_next_focus_out_event)
 		rcw_keyboard_ungrab(cnnwin);
-	else
-		cnnwin->priv->dont_ungrab_on_next_focus_out_event = FALSE;
+
+	cnnwin->priv->dont_ungrab_on_next_focus_out_event = FALSE;
 	cnnwin->priv->hostkey_activated = FALSE;
 	if (!(cnnobj = rcw_get_visible_cnnobj(cnnwin))) return;
 
